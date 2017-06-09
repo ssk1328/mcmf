db0a3007fb6e59d07a3f1224cf610c83273f29ed
Shashank Gangrade <ssk1328@users.noreply.github.com>
2017-06-09 18:58:03 +0530

    cleanup in mcflow.py files

--- mcflow.py
+++ mcflow.py
@@ -1,259 +1,197 @@
 from gurobipy import *
-
 import math
 import sys
 import copy
 
+def read_placement_files () :
+	placementAll = {}
+	for p in p_values:
+		fileobj = open("qap_scripts/qap_sol_pg"+ str(p) +".txt")
+		file_string = fileobj.read()
+		placement_list = map(int, file_string.split())
+		placementAll[p] = placement_list
+		fileobj.close()
+
+	return placementAll
+
+def getDependencyList():
+	## This list basically means ith nodes is connected to pg nodes mentioned in the list dependency[i]
+	dependency = []
+	# Inserting the connections between Point Node i and all the Line nodes it is connected to, except itself 
+	for i in range(numCores):
+		dependency.append([])
+		for j in incidence:  
+			if (i != (i+ j)%numCores ):
+				dependency[i].append((j+i)%numCores) 
+
+	# Inserting the connections between Line node i and all the Point nodes it is connected to, except itself
+	for i in range(numCores):
+		for j in incidence:  
+			if (i != (i - j)% numCores ):
+				dependency[i].append((i - j)% numCores)
+
+	return dependency
+	#dependency_out = []
+	# Inserting the connections between Point Node i and all the Line nodes it is connected to, except itself 
+	#for i in range(numCores):
+	#    dependency_out.append([])
+	#    for j in incidence:  
+	#        if (i != (i+ j)%numCores ):
+	#            dependency_out[i].append((j+i)%numCores) 
+
+	#dependency_in = []
+	# Inserting the connections between Line node i and all the Point nodes it is connected to, except itself
+	#for i in range(numCores):
+	#    dependency_in.append([])
+	#    for j in incidence:  
+	#        if (i != (i - j)% numCores ):
+	#            dependency_in[i].append((i - j)% numCores)
+
+	# print "Data sent at t=0, x0, x1, ...: "
+	# print "Dependency_out: "
+	# print dependency_out 
+
+	# print "Dependency sent after t = t1, y0part0, y1part0, ..... ",
+	# print "Dependency_in: "
+	# print dependency_in 
+
+
+def init_commodity():
+	commodities = [] # start with empty list for commodities
+	for i in range(len(dependency)):
+		for j in range(len(dependency[i])):
+			commodities.append('s'+ str(i) +'d'+ str(dependency[i][j]))
+
+	return commodities
+
+def init_nodes():
+	nodes = [] # start with empty list for nodes
+
+	for i in range(len(placement)):
+		string = 'p'+str(placement[i])+'m'+str(i)
+		nodes.append(string)
+	return nodes
+
+def init_inflow():
+	inflow = {} # start with empty dictionary for inflow specification
+
+	for i in commodities:
+
+		index_s = i.index('s') + 1
+		index_d = i.index('d')
+		index_d1 = i.index('d') + 1
+		index_end = len(i)
+
+		src = int(i[index_s:index_d])
+		dest = int(i[index_d1:index_end])
+
+		for j in nodes:
+
+			n_index_p = j.index('p')
+			n_index_m = j.index('m')
+
+			p_num = j[n_index_p:n_index_m]
+
+			if   p_num == 'p'+str(src):
+				inflow[(i, j)] = PACK_LENGTH 
+			elif p_num == 'p'+str(dest):
+				inflow[(i, j)] = -PACK_LENGTH
+			else :
+				inflow[(i, j)] = 0 
+
+	return inflow
+
+def init_arc_list():
+	arc = []
+	for i in range(numMeshnodes):
+		if i >= meshEdge : #up
+			arc.append(( 'p'+str(placement[i])+'m'+str(i), 'p'+ str(placement[i-meshEdge]) +'m'+str(i-meshEdge)))
+		if i%meshEdge > 0 : #left
+			arc.append(( 'p'+str(placement[i])+'m'+str(i), 'p'+ str(placement[i-1]) +'m'+str(i-1)))
+		if i < numMeshnodes - meshEdge :  # down
+			arc.append(( 'p'+str(placement[i])+'m'+str(i), 'p'+ str(placement[i+meshEdge]) +'m'+str(i+meshEdge)))
+		if i%meshEdge < meshEdge-1 :  # right
+			arc.append(( 'p'+str(placement[i])+'m'+str(i), 'p'+ str(placement[i+1]) +'m'+str(i+1)))
+
+	return arc
+
+def init_cost():
+	cost = {}
+	for i in commodities:
+		for j in arc:
+			j_list = list(j)
+			j_tup = [i]+j_list
+			tup = tuple(j_tup)
+			cost[tup] = 1
+	return cost
+
+# ------------------------------------------------------------------------------------------------------------------ #
+# MAIN SCRIPT STARTS HERE 
+# ------------------------------------------------------------------------------------------------------------------ #
+
 p = 2	# Size of pg, this was specified from command line
-print "Size of pg, p is %d"%p 
+p_values  = [2, 3, 4, 5, 7, 8, 9, 11, 13, 16, 17, 19]
 
 numPGnodes = p*p +p +1
 numCores = numPGnodes
-print "Number of cores in pg is %d " %numCores
-#Decide Mesh Size 
+# Decide Mesh Size here, largest perfect square number greater than numCores
 meshEdge = 1
 numMeshnodes = meshEdge * meshEdge
 while (numPGnodes > numMeshnodes):
     meshEdge += 1
     numMeshnodes = meshEdge *meshEdge
-print "numMeshnodes is %d"%numMeshnodes
-print "meshEdge is %d"%meshEdge
 
 
 ## This is the inflow and outflow at source and destination respectively for all comoodities
 ## This represents how many parts the packet can be broken into
-pack_lenght = 1;
+PACK_LENGTH = 8;
 
 ## This is the fixed capacity for all arcs
 ## This has to be changed to get a feasible soluton usually
-capacity_const = 2;
+CAPACITY_CONST = 16;
 
-incidenceAll = {2 : [0,1,3], 
-                3 : [0,1,3,9],               
-                4 : [0,1,4,14,16],           
-                5 : [0,1,3,8,12,18],         
-                7 : [0,1,3,13,32,36,43,52],  
-                8 : [0,1,3,7,15,31,36,54,63]}
+incidenceAll = {2  : [0,1,3], 
+                3  : [0,1,3,9],               
+                4  : [0,1,4,14,16],           
+                5  : [0,1,3,8,12,18],         
+                7  : [0,1,3,13,32,36,43,52],  
+                8  : [0,1,3,7,15,31,36,54,63],
+                9  : [0,1,3,9,27,49,56,61,77,81],
+                11 : [0,1,3,12,20,34,38,81,88,94,104,109],
+                13 : [0,1,3,16,23,28,42,76,82,86,119,137,154,175],
+                16 : [0,1,3,7,15,31,63,90,116,127,136,181,194,204,233,238,255],
+                17 : [0,1,3,30,37,50,55,76,98,117,129,133,157,189,199,222,293,299],
+                19 : [0,1,19,28,96,118,151,153,176,202,240,254,290,296,300,307,337,361,366,369]
+                }
 
 incidence = incidenceAll[p] 
 
-#print "Incidence:",
-#print incidence 
-
-
 ###############################################################################
-## Add code to read placement routing here, this is using simulated annealing 
-## based quadratic assignment problem solver
-## Add code to read this from file later, add code to print this in a file in qap solver cpp file later
-
-
-placementAll = {
-    2: [8, 4, 7, 1, 5, 2, 0, 6, 3] ,
-    3: [13, 1, 11, 2, 14, 0, 10, 12, 9, 5, 8, 6, 15, 4, 7, 3] 
-}
-
+placementAll = read_placement_files()
 placement = placementAll[p]
-
+print "placement", placement
+## 2: [8, 4, 7, 1, 5, 2, 0, 6, 3] , TODO: Fix this
+placement = [8, 4, 7, 1, 5, 2, 0, 6, 3] 
 ## This means that at the ith location in mesh we put up the placement[i]th processor
 ## Example here processor 8 is placed at 0th location in mesh
 ## Example here processor 4 is placed at 1st location in mesh
 
-## Dependency generically means the nodes with which given node communicates. In matrix vector product, we 
-## communicate only with the Nodes containing : Line Nodes of the given point node and the point nodes of the given 
-## line node.
-## Hence : 
-## Dependency contains the list of nodes which need to be close to a node in Mesh (Basically list of Line Nodes 
-## connected to Point Node and list of Point Nodes connected to Line Node, for a given node)
-
-dependency = []
-# Inserting the connections between Point Node i and all the Line nodes it is connected to, except itself 
-for i in range(numCores):
-    dependency.append([])
-    for j in incidence:  
-        if (i != (i+ j)%numCores ):
-            dependency[i].append((j+i)%numCores) 
-
-# Inserting the connections between Line node i and all the Point nodes it is connected to, except itself
-for i in range(numCores):
-    for j in incidence:  
-        if (i != (i - j)% numCores ):
-            dependency[i].append((i - j)% numCores)
-
-print "Dependency:"
-## This list basically means ith nodes is connected to pg nodes mentioned in ith row
-for i in range(len(dependency)):
-	print dependency[i] 
+## Actual output from sa_qap is different 
+## In the list generated, the ith value in the list is the position of ith processor in mesh
 
 
-
-dependency_out = []
-# Inserting the connections between Point Node i and all the Line nodes it is connected to, except itself 
-for i in range(numCores):
-    dependency_out.append([])
-    for j in incidence:  
-        if (i != (i+ j)%numCores ):
-            dependency_out[i].append((j+i)%numCores) 
-
-dependency_in = []
-# Inserting the connections between Line node i and all the Point nodes it is connected to, except itself
-for i in range(numCores):
-    dependency_in.append([])
-    for j in incidence:  
-        if (i != (i - j)% numCores ):
-            dependency_in[i].append((i - j)% numCores)
-
-print "Data sent at t=0, x0, x1, ...: "
-print "Dependency_out: "
-print dependency_out 
-
-print "Dependency sent after t = t1, y0part0, y1part0, ..... ",
-print "Dependency_in: "
-print dependency_in 
-
-
-#Setting up the commTraffic matrix
-commTraffic = [[0] * numCores for i in range(numCores)]
-
-for i in range(numCores):
-    for j in dependency[i]:
-        srclines = [(x+i)%numCores for x in incidence]
-        destlines = [(x+j)%numCores for x in incidence]
-        matchline = 0
-        for a in srclines:
-            done = 0 # to indicate whether matched or not
-            for b in destlines:
-                if a == b:
-                    done =1
-                    matchline = a #src and dest lines matched
-                    break
-            if done is 1:
-                break
-        commTraffic[i][matchline] += 1
-        commTraffic[matchline][j] += 1
-
-print "Communication traffic also flow matirx:"
-print commTraffic
-
-
-## From here on we tackle the problem at time t=t1 and hence use the dependency_in from above
-## For time t=0, use dependency_out from abbove
-
-commodities = [] # start with empty list for commodities
-for i in range(len(dependency)):
-	for j in range(len(dependency[i])):
-		commodities.append('s'+ str(i) +'d'+ str(dependency[i][j]))
-
-print "commodities:"
-print commodities
-
-nodes = [] # start with empty list for nodes
-
-for i in range(len(placement)):
-    string = 'p'+str(placement[i])+'m'+str(i)
-    nodes.append(string)
-
-#   for i in range(len(dependency_in)):
-#	  nodes.append('p'+str(i))
-
-print "nodes:"
-print nodes
-
-inflow = {} # start with empty dictionary for inflow specification
-
-for i in commodities:
-
-	index_s = i.index('s') + 1
-	index_d = i.index('d')
-	index_d1 = i.index('d') + 1
-	index_end = len(i)
-
-	src = int(i[index_s:index_d])
-	dest = int(i[index_d1:index_end])
-
-	for j in nodes:
-
-		n_index_p = j.index('p')
-		n_index_m = j.index('m')
-
-		p_num = j[n_index_p:n_index_m]
-
-		if   p_num == 'p'+str(src):
-			inflow[(i, j)] = pack_lenght 
-		elif p_num == 'p'+str(dest):
-			inflow[(i, j)] = -pack_lenght
-		else :
-			inflow[(i, j)] = 0 
-
-print "Inflow: "
-print inflow
-print "Lenght of inflow dictionary: "
-print len(inflow)
-
-print " "
-print " "
-print " "
-
-print "numMeshnodes is %d"%numMeshnodes
-print "meshEdge is %d"%meshEdge
-
-arc = []
-for i in range(numMeshnodes):
-	if i >= meshEdge : #up
-		arc.append(( 'p'+str(placement[i])+'m'+str(i), 'p'+ str(placement[i-meshEdge]) +'m'+str(i-meshEdge)))
-	if i%meshEdge > 0 : #left
-		arc.append(( 'p'+str(placement[i])+'m'+str(i), 'p'+ str(placement[i-1]) +'m'+str(i-1)))
-	if i < numMeshnodes - meshEdge :  # down
-		arc.append(( 'p'+str(placement[i])+'m'+str(i), 'p'+ str(placement[i+meshEdge]) +'m'+str(i+meshEdge)))
-	if i%meshEdge < meshEdge-1 :  # right
-		arc.append(( 'p'+str(placement[i])+'m'+str(i), 'p'+ str(placement[i+1]) +'m'+str(i+1)))
-
-
-print arc
-print "Length of Arc: ", 
-print len(arc)
+dependency = getDependencyList()
+commodities = init_commodity()
+nodes = init_nodes()
+inflow = init_inflow()
+arc = init_arc_list()
 
 multi_dict = {}
-
-
-
-################################
-################################
-## Change Capacities here
-
-
 for i in arc:
-	multi_dict[i] =  capacity_const	# Change to get a feasible solution
+	multi_dict[i] =  CAPACITY_CONST
 
 arcs, capacity = multidict(multi_dict)
-
-
-print "All arcs after: "
-print type(arcs)
-print arcs
-
-# print "All capacities after: "
-# print capacity
-
-cost = {}
-
-for i in commodities:
-	for j in arc:
-#		print j
-		j_list = list(j)
-#		print j_list
-		#j_tup = j_list.insert(0,i)
-		j_tup = [i]+j_list
-#		print i
-#		print j_tup
-		tup = tuple(j_tup)
-		cost[tup] = 1
-
-print ""
-print ""
-print ""
-
-# print "Cost: "
-# print cost
+cost = init_cost()
 
 m = Model('netflow')
 
@@ -264,21 +202,13 @@ flow = m.addVars(commodities, arcs, obj=cost, name="flow")
 m.addConstrs(
     (flow.sum('*',i,j) <= capacity[i,j] for i,j in arcs), "cap")
 
-# Equivalent version using Python looping
-# for i,j in arcs:
-#   m.addConstr(sum(flow[h,i,j] for h in commodities) <= capacity[i,j],
-#               "cap[%s,%s]" % (i, j))
-
-
 # Flow conservation constraints
 m.addConstrs(
     (flow.sum(h,'*',j) + inflow[h,j] == flow.sum(h,j,'*')
     for h in commodities for j in nodes), "node")
-# Alternate version:
-# m.addConstrs(
-#   (quicksum(flow[h,i,j] for i,j in arcs.select('*',j)) + inflow[h,j] ==
-#     quicksum(flow[h,j,k] for j,k in arcs.select(j,'*'))
-#     for h in commodities for j in nodes), "node")
+
+# Dont print log to console
+m.Params.LogToConsole = 0
 
 # Compute optimal solution
 m.optimize()
